7449a84aedf5b06eba13cf63d8f1f60fe0e9e062
Serdar Ashirov <webapis@users.noreply.github.com>
2023-05-08 09:25:37 +0500

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,10 @@
 const path = require("path");
 module.exports = {
 	i18n: {
-		locales: ["en", "de", "es", "ar", "he", "zh","tr"],
+		locales: ["en","tr"],
 		defaultLocale: "en",
 		localeDetection: false,
 	},
 	localePath: path.resolve("./public/locales"),
 };
+//	locales: ["en", "de", "es", "ar", "he", "zh","tr"],
