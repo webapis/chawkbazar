fd14cf3f633a8dac566f71a98e0354fe7610a6dc
Serdar Ashirov <webapis@users.noreply.github.com>
2023-05-08 15:38:08 +0500

    cc

--- src/framework/basic-rest/utils/http.ts
+++ src/framework/basic-rest/utils/http.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./get-token";
 
 const http = axios.create({
-//  baseURL: process.env.NEXT_PUBLIC_REST_API_ENDPOINT,
+  baseURL: process.env.NEXT_PUBLIC_REST_API_ENDPOINT,
   timeout: 30000,
   headers: {
     Accept: "application/json",
