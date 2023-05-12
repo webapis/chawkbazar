1f1d207cc565a1835a8664ed18b46b55e80ab0a1
Serdar Ashirov <webapis@users.noreply.github.com>
2023-05-12 11:58:30 +0500

    c

--- utils/mapPrice.mjs
+++ utils/mapPrice.mjs
@@ -2,64 +2,82 @@ function mapPrice(price) {
 
 
     try {
-        const trimPrice = price.replace('TL', '').replace('$', '').trim()
+        const trimPrice = price.replace('TL', '').replace('$', '').trim()//.replace(/[a-z]/gi, '') 
 
         switch (true) {
 
             case /^\d\d\d[,]\d\d$/.test(trimPrice)://299,99
-                
-                return  parseFloat(trimPrice.replace(',', '.')).toFixed(2)
-            case /^\d\d\d[,]\d$/.test(trimPrice)://299,9
-                
+
                 return parseFloat(trimPrice.replace(',', '.')).toFixed(2)
+            case /^\d\d\d[,]\d$/.test(trimPrice)://299,9
+
+                return parseFloat(trimPrice.replace(',', '.')).toFixed(2)
+            case /^\d\d\d[.]\d$/.test(trimPrice)://299.9
+                return parseFloat(trimPrice).toFixed(2)
             case /^\d\d\d[.]\d\d$/.test(trimPrice)://299.99
-                
+
                 return parseFloat(trimPrice).toFixed(2)
             case /^\d\d\d\d[.]\d\d$/.test(trimPrice)://1499.99
-                
+
                 return parseFloat(trimPrice).toFixed(2)
+            case /^\d\d\d\d[.]\d$/.test(trimPrice)://1499.9
+                return parseFloat(trimPrice).toFixed(2)
+            case /^\d\d\d\d[,]\d$/.test(trimPrice)://1499,9
+                return parseFloat(trimPrice.replace(',', '.')).toFixed(2)
             case /^\d\d\d\d[,]\d\d$/.test(trimPrice)://1499,99
-                
+
                 return parseFloat(trimPrice.replace(',', '.')).toFixed(2)
             case /^\d[.]\d\d\d[,]\d\d$/.test(trimPrice)://1.449,90
                 return parseFloat(trimPrice.replace('.', '').replace(',', '.')).toFixed(2)
 
             case /^\d[.]\d\d\d$/.test(trimPrice)://1.449
-                
+
                 return parseFloat(trimPrice.replace('.', '')).toFixed(2)
             case /^\d\d\d\d$/.test(trimPrice)://4299
-                
+
                 return parseFloat(trimPrice).toFixed(2)
             case /^\d[,]\d\d\d[.]\d\d$/.test(trimPrice)://3,950.00
-                
+
                 return parseFloat(trimPrice.replace(',', '')).toFixed(2)
             case /^\d\d\d$/.test(trimPrice)://999
-                
+
                 return parseFloat(trimPrice).toFixed(2)
             case /^\d\d[,]\d\d$/.test(trimPrice)://81,00
-                
+
                 return parseFloat(trimPrice.replace(',', '.')).toFixed(2)
             case /^\d\d[.]\d\d$/.test(trimPrice)://81.00
-                
+
+                return parseFloat(trimPrice).toFixed(2)
+            case /^\d\d[.]\d$/.test(trimPrice)://99.9
+
+                return parseFloat(trimPrice).toFixed(2)
+
+
+            case /^\d\d[,]\d$/.test(trimPrice)://99,9
+
+                return parseFloat(trimPrice.replace(',', '.')).toFixed(2)
+
+            case /^\d\d$/.test(trimPrice)://99
+
                 return parseFloat(trimPrice).toFixed(2)
             case /^\d\d[.]\d\d\d[,]\d\d$/.test(trimPrice)://14.918,00
-                
+
                 return parseFloat(trimPrice.replace('.', '').replace(',', '.')).toFixed(2)
             case /^\d\d\d\d\d$/.test(trimPrice)://11499
-                
+
                 return parseFloat(trimPrice).toFixed(2)
             case /^\d\d[.]\d\d\d$/.test(trimPrice)://14.918
-                
+
                 return parseFloat(trimPrice.replace('.', '')).toFixed(2)
             case /^\d\d[,]\d\d\d$/.test(trimPrice)://14,918
-                
+
                 return parseFloat(trimPrice.replace(',', '')).toFixed(2)
             default:
-                
+
                 return parseFloat(0).toFixed(2)
         }
     } catch (error) {
-        
+
         console.log('price----', price)
         throw error
     }
--- utils/script copy.mjs
+++ utils/script copy.mjs
@@ -13,9 +13,7 @@ const prisma = new PrismaClient()
 
 
 let filePaths = []
-debugger
-const deleted =await prisma.products.deleteMany({ where: { imageUrl: {contains:'kadin-kurklu-suet-pem-kadin-gri-kurklu-5109d7.jpeg'} } })
-
+await prisma.products.deleteMany({})
 debugger
 walkSync(path.join(process.cwd(), `erkek/unzipped-data`), async (filepath) => {
   filePaths.push(filepath)
@@ -31,26 +29,6 @@ walkSync(path.join(process.cwd(), `erkek-cocuk/unzipped-data`), async (filepath)
   filePaths.push(filepath)
 })
 
-// const data = [{
-//   "title": "abiyefon gümüş simli gece çantası sh806 _kadin",
-//   "priceNew": "77.9",
-//   "imageUrl": "images/image/289/22712/113557762_0.jpg",
-//   "link": "gumus-simli-gece-cantasi-sh806",
-//   "timestamp": "1683692507642",
-//   "marka": "abiyefon",
-//   "gender": "_kadin",
-//   "modified": 1683692507642
-// },
-// {
-//   "title": "abiyefon beyaz simli gece çantası sh806 _kadin",
-//   "priceNew": "99.9",
-//   "imageUrl": "images/image/289/22712/113557758_0.jpg",
-//   "link": "beyaz-simli-gece-cantasi-sh806",
-//   "timestamp": "1683866882898",
-//   "marka": "abiyefon",
-//   "gender": "_kadin",
-//   "modified": 1683866882898
-// }].map(m => { return { ...m, modified: new Date(m.modified) } })
 let list = []
 let sliceCounter = 0
 let isComplete = false
@@ -58,17 +36,15 @@ let indexCounter = 0
 
 while (!isComplete) {
 
+
   for (let filepath of filePaths) {
 
     const raw = fs.readFileSync(filepath, { encoding: 'utf-8' })
-    const data = JSON.parse(raw).map(m => { return { ...m, modified: new Date(), priceNew: m.priceNew ? m.priceNew.toString() : m.priceNew, timestamp: m.timestamp.toString(), price: m.priceNew ? mapPrice(m.priceNew.toString()) : 0 } }).slice(sliceCounter, sliceCounter + 20)
-    const removeImgNull = data.filter(m => m.imageUrl !== null)
-    const imageUrlWithNull = data.filter(m => m.imageUrl === null)
-    if (imageUrlWithNull.length > 0) {
-      console.log('imageUrlWithNull', imageUrlWithNull.length)
-    }
+    const data = JSON.parse(raw).map(m => { return { ...m, priceNew: m.priceNew ? m.priceNew.toString() : m.priceNew, timestamp: m.timestamp.toString(), price: m.priceNew ? mapPrice(m.priceNew.toString()) : 0 } }).slice(sliceCounter, sliceCounter + 20)
 
-    list.push(...removeImgNull)
+    list.push(...data)
+
+
 
   }
   if (list.length > 0) {
@@ -80,7 +56,7 @@ while (!isComplete) {
       const navs = genegateNavigation({ title: l.title })
       // const ft = l.priceNew.replace('TL', '').replace('$', '').replaceAll('.','').replaceAll(',','').trim()
       // const red =formatNumber(ft, {precision:1})
-
+   
       listwithNav.push({ ...l, ...navs })
     }
 
@@ -103,8 +79,6 @@ while (!isComplete) {
   } else {
     debugger
     isComplete = true
-    const deleteResult = await prisma.products.deleteMany({ where: { modified: { lt: new Date(new Date().setHours(0, 0, 0, 0)) } } })
-    console.log('deleteResult', deleteResult)
   }
 
 
@@ -119,13 +93,11 @@ debugger
 async function main({ data }) {
 
   try {
-    for (let d of data) {
+    for(let d of data ){
       debugger
-
       const user = await prisma.products.upsert({
-        where: {
-          imageUrl: d.imageUrl
-        },
+        where: { 
+          imageUrl },
         update: d,
         create: d,
       })
@@ -133,11 +105,11 @@ async function main({ data }) {
     }
 
     // const user = await prisma.products.createMany({ data })
-
+  
 
     await prisma.$disconnect()
   } catch (error) {
-    console.error(error)
+    console.error(error, data)
 
     await prisma.$disconnect()
 
